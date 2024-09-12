a00889e2fac8d29e8260d212aa66f62948880261
erzaginting <168664390+erzaginting@users.noreply.github.com>
2024-09-12 12:10:12 +0700

    Delete check_data_pelanggan_lakilaki_jeniskamar.sql

--- check_data_pelanggan_lakilaki_jeniskamar.sql
+++
@@ -1,9 +0,0 @@
--- CHECK DATA PELANGGAN BERJENIS KELAMIN LAKI LAKI YANG MEMESAN KAMAR SUITE DAN DELUXE
-
-
-SELECT p.pelanggan_id, p.nama, p.tanggal_lahir, p.jenis_kelamin, p.alamat, p.email, p.no_telepon, k.jenis_kamar
-FROM tb_pelanggan p
-JOIN tb_reservasi_hotel r ON p.pelanggan_id = r.pelanggan_id
-JOIN tb_kamar k ON r.kamar_id = k.kamar_id
-WHERE p.jenis_kelamin = 'Laki-laki' 
-AND k.jenis_kamar IN ('Deluxe', 'Suite');

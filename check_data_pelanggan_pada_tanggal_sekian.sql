f497969f533c4adb36204c47dbe104ecfbbd1062
erzaginting <168664390+erzaginting@users.noreply.github.com>
2024-09-12 12:10:25 +0700

    Delete check_data_pelanggan_pada_tanggal_sekian.sql

--- check_data_pelanggan_pada_tanggal_sekian.sql
+++
@@ -1,7 +0,0 @@
--- CHECK DATA PELANGGAN PADA TANGGAL 2024-09-01 - 2024-09-25 
-
-
-SELECT p.pelanggan_id, p.nama, p.tanggal_lahir, p.jenis_kelamin, p.alamat, p.email, p.no_telepon, r.tanggal_check_in, r.tanggal_check_out
-FROM tb_pelanggan p
-JOIN tb_reservasi_hotel r ON p.pelanggan_id = r.pelanggan_id
-WHERE r.tanggal_check_in BETWEEN '2024-09-01' AND '2024-09-25';

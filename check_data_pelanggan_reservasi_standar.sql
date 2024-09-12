ecc165570de5af3cc4ff541aefb4596e10359ac2
erzaginting <168664390+erzaginting@users.noreply.github.com>
2024-09-12 12:10:50 +0700

    Delete check_data_pelanggan_reservasi_standar.sql

--- check_data_pelanggan_reservasi_standar.sql
+++
@@ -1,8 +0,0 @@
--- CHECK DATA PENGRESERVASI JENIS KAMAR STANDAR
-
-
-SELECT p.pelanggan_id, p.nama, p.tanggal_lahir, p.jenis_kelamin, p.alamat, p.email, p.no_telepon, k.jenis_kamar
-FROM tb_pelanggan p
-JOIN tb_reservasi_hotel r ON p.pelanggan_id = r.pelanggan_id
-JOIN tb_kamar k ON r.kamar_id = k.kamar_id
-WHERE k.jenis_kamar = 'Standard';

03b6877943d7038c0bad6829b83e45fc8a412764
erzaginting <168664390+erzaginting@users.noreply.github.com>
2024-09-12 12:10:01 +0700

    Delete check_data_biaya_pelanggan_terbanyak.sql

--- check_data_biaya_pelanggan_terbanyak.sql
+++
@@ -1,7 +0,0 @@
--- CHECK DATA PELANGGAN DENGAN PENGELUARAN TERBANYAK ( 9000000 )
-
-
-SELECT p.pelanggan_id, p.nama, p.tanggal_lahir, p.jenis_kelamin, p.alamat, p.email, p.no_telepon, r.total_biaya
-FROM tb_pelanggan p
-JOIN tb_reservasi_hotel r ON p.pelanggan_id = r.pelanggan_id
-WHERE r.total_biaya = 9000000;
